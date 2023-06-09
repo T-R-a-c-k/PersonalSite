a7ce9fe28a18d88b8126a977817f6c537cfaa7e9
T-R-a-c-k <117862440+VisforVirus@users.noreply.github.com>
2023-06-09 12:48:24 -0400

    More random information

--- personal-site/src/components/profile.tsx
+++ personal-site/src/components/profile.tsx
@@ -30,7 +30,7 @@ const changeLetters = (event: React.MouseEvent) => {
       wordArray.splice(0, 1);
     }
     count++;
-  }, 20);
+  }, 33);
 
   //Clear this at the end to stop the scramble
   setTimeout(() => {
@@ -49,27 +49,103 @@ const Profile: React.FunctionComponent<ProfileProps> = () => {
             <div className={Styles.screenContent}>Github: T-R-a-c-k</div>
           </div>
           <div className={Styles.barLeftItem} onClick={(e) => changeLetters(e)}>
-            About Me
+            <h1>About Me</h1>
+            <p>something</p>
           </div>
           <div className={Styles.barLeftItem} onClick={(e) => changeLetters(e)}>
-            Contact related stuff
+            <h1>Contact</h1>
+            <p>Email: tylerrack@rogers.com</p>
+            <p>Phone: 289-893-2280</p>
+            <p>LinkedIn: tylerrack16</p>
           </div>
-          <div className={Styles.barLeftItem} onClick={(e) => changeLetters(e)}>
-            Contact related
-            stuffewruiqwefqgweqwefgqwhg12341234312341234123123121321323123121321323efvqwhgfeqjwefh
-          </div>
+          <a href="https://github.com/T-R-a-c-k" color="red">
+            GitHub: T-R-a-c-k
+          </a>
         </div>
         <div className={Styles.barRight}>
           <div className={Styles.barRightItem}>
             <h1>Education</h1>
-            <p>Something</p>
+            <div className={Styles.barRightItemHeading}>
+              <p>Oshawa</p>
+              <p>Ontario Tech University</p>
+              <p>2018 - 2022</p>
+            </div>
+            <p>BE(Hons) in Networking and Security</p>
+            <p>GPA: 4.0 (Current Year and Total)</p>
           </div>
           <div className={Styles.barRightItem}>
             <h1>Employment</h1>
-            <h4>Web Developer Internship </h4>
+            <div className={Styles.barRightItemHeading}>
+              <p>Web Developer Internship</p>
+              <p> Southlake Regional Hospital</p>
+              <p>2020 - 2022</p>
+            </div>
+            <p>
+              Addressed regular production bugs and improvements in existing
+              <b> Java</b> services using <b>Kanban</b> to prioritize
+              requirements, and utilizing <b>CI / CD</b> pipelines to rapidly
+              iterate.
+            </p>
+            <p>
+              Create and tested <b>RESTFUL API</b> endpoints with
+              <b> Java Spring</b>.
+            </p>
+            <p>
+              Updated and tested various <b>React</b> components to increase
+              efficiency of <b>API calls</b>.
+            </p>
+            <p>
+              Used <b>Datadog</b> to monitor and debug production systems.
+            </p>
+            <p>
+              Worked within a <b> scrum team</b> to develop solutions.
+            </p>
           </div>
-          <div className={Styles.barRightItem}>Projects</div>
-          <div className={Styles.barRightItem}>Technical Skills</div>
+          <div className={Styles.barRightItem}>
+            <h1>Projects</h1>
+
+            <div className={Styles.barRightItemHeading}>
+              <p>
+                <a
+                  href="https://github.com/T-R-a-c-k/MERN"
+                  style={{ color: "red" }}
+                >
+                  MERN Stack
+                </a>
+              </p>
+            </div>
+            <p>
+              Created full specified REST backend built with{" "}
+              <b>Node.js, Express.js and MongoDB</b> following the <b>MVC</b>{" "}
+              architecture.
+            </p>
+            <p>
+              Made test suites using <b>Jest and Supertest</b>
+            </p>
+            <p>
+              Developed a multi page application with React and
+              <b> React-bootstrap</b>
+            </p>
+          </div>
+          <div className={Styles.barRightItem}>
+            <h1>Technical Skills</h1>
+
+            <div className={Styles.barRightItemHeading}>
+              <p>Languages</p>
+            </div>
+            <p>Java, Python, Javascript / HTML / CSS, C++, SQL, Bash </p>
+            <br></br>
+
+            <div className={Styles.barRightItemHeading}>
+              <p>Frameworks and Libraries</p>
+            </div>
+            <p>Jest, Node.js, MongoDB, Express.js, React, Spring, Bootstrap</p>
+            <br></br>
+            <div className={Styles.barRightItemHeading}>
+              <p>Tools</p>
+            </div>
+            <p>Git, Linux, Docker, Kubernates. CI/CD, JWT</p>
+          </div>
         </div>
       </div>
     </React.Fragment>
