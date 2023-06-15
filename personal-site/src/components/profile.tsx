19dcaca14e81e58e4e2263b4f56d9f0fe5672da4
T-R-a-c-k <117862440+VisforVirus@users.noreply.github.com>
2023-06-15 14:25:44 -0400

    Put all left / top values into inline CSS to allow for manipulation

--- personal-site/src/components/profile.tsx
+++ personal-site/src/components/profile.tsx
@@ -3,18 +3,52 @@ import { Link } from "react-router-dom";
 import Styles from "../css/profile.module.css";
 import { changeLetters } from "../functions/changeLetters";
 import Draggable from "react-draggable";
-import { link } from "fs";
 
 interface ProfileProps {}
 
+type Displacement = {
+  top: number;
+  left: number;
+};
+
+const educationDisplacement: Displacement = {
+  top: 10.4,
+  left: 29.3,
+};
+
+const employementDisplacement: Displacement = {
+  top: 38.2,
+  left: 35.8,
+};
+
+const projectDisplacement: Displacement = {
+  top: 67.5,
+  left: 48,
+};
+
+const technicalSkillsDisplacement: Displacement = {
+  top: 27,
+  left: 74.3,
+};
+
+const ROTATOR_LINK_DISPLACEMENT: Array<Displacement> = [
+  educationDisplacement,
+  employementDisplacement,
+  projectDisplacement,
+  technicalSkillsDisplacement,
+];
+
 const fixRotators = () => {
+  const BASE_SIZE = -750;
   const links = document.querySelectorAll("a");
-  let rotators = Array.from(links);
-  rotators = rotators.filter((element) => {
-    return element.className.includes("profile_");
+  let rotatorLinks = Array.from(links);
+  rotatorLinks = rotatorLinks.filter((element) => {
+    return element.className.includes("Link");
   });
-  rotators.forEach((element) => {
-    element.style.left = "3%";
+
+  rotatorLinks.forEach((element, index) => {
+    element.style.top = `${ROTATOR_LINK_DISPLACEMENT[index].top}%`;
+    element.style.left = `${ROTATOR_LINK_DISPLACEMENT[index].left}%`;
   });
 };
 
@@ -39,9 +73,11 @@ const Profile: React.FunctionComponent<ProfileProps> = () => {
 
   //Height should be about 1.7~ times bigger than width
   //This allows every configuration to keep the picture within the parent
+  const leftBound = -1 * windowSize[0] + windowSize[0] / 3;
+  const topBound = (-1 * windowSize[0] + windowSize[0] / 3) / 1.7;
   const boundValues = {
-    left: -1 * windowSize[0] + windowSize[0] / 3,
-    top: (-1 * windowSize[0] + windowSize[0] / 3) / 1.7,
+    left: leftBound,
+    top: topBound,
     right: -5,
     bottom: 0,
   };
@@ -70,7 +106,11 @@ const Profile: React.FunctionComponent<ProfileProps> = () => {
           </div>
         </div>
         <div className={Styles.barRight}>
-          <Draggable bounds={boundValues} disabled={windowSize[0] <= 800}>
+          <Draggable
+            bounds={boundValues}
+            disabled={windowSize[0] <= 800}
+            cancel="a"
+          >
             <div className={Styles.barRightImage}>
               <div>
                 <h2>Width: {boundValues.left}</h2>
@@ -81,6 +121,7 @@ const Profile: React.FunctionComponent<ProfileProps> = () => {
                       <p>Education</p>
                     </div>
                   </Link>
+
                   <Link to="/employement" className={Styles.employementLink}>
                     <div id={Styles.rotator}>
                       <p>Employement</p>
